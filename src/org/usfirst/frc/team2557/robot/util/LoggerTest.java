91fd81ec67b41afd7a6dd014fca7ac824ce1540a
Matt Miller <blacktop19@hotmail.com>
2018-02-06 19:37:29 -0800

    Add comments and update the test a tiny bit.

--- src/org/usfirst/frc/team2557/robot/util/LoggerTest.java
+++ src/org/usfirst/frc/team2557/robot/util/LoggerTest.java
@@ -3,9 +3,15 @@ package org.usfirst.frc.team2557.robot.util;
 public class LoggerTest {
 
 	public static void main(String[] args) {
+		// Set the log file name to something other than the default.
+		Logger.logFileName = "LoggerTest.txt";
+		
 		for (int i = 0; i < 5; i++) {
+			// Each iteration increases the log level the logger will log messages at.
 			Logger.currentLogLevel = i;
+			
 			for (int j = 0; j < 5; j++) {
+				// Test logging at each level for this log level.
 				Logger.LogMessage(j, "Logging log level: \"" + j + "\" with current level at: \"" + i + "\"");
 			}
 		}
